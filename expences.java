dc10a74fc655c30dd2515a1c9683e60ce837aa0d
Varun Devata <77072496+varun-devata@users.noreply.github.com>
2022-09-16 23:54:43 +0530

    Add files via upload

---
+++ expences.java
@@ -0,0 +1,20 @@
+/*
+ * Write a program to calculate the total expenses. Quantity and price per item are input by the user and discount of 10% is offered if the expense is more than 5000.
+ */
+
+
+import java.util.Scanner;
+
+public class expences {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int quantity = sc.nextInt();
+        float pricePerItem = sc.nextFloat();
+        double totalExpences = quantity * pricePerItem;
+
+        if(totalExpences > 5000) {
+        	totalExpences -= totalExpences * 0.1;
+        }
+        System.out.println(totalExpences);
+    }
+}
